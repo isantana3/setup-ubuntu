ff2b6eaa2fa0aaea7b3d3724ae259cb55b243b8d
Iago Gomes <42523010+isantana3@users.noreply.github.com>
2022-10-16 14:09:57 -0300

    Update Standard_config.sh

--- Standard_config.sh
+++ Standard_config.sh
@@ -1,49 +1,49 @@
 ## Update
-$ sudo apt update
+sudo apt update
 
 ## install snap
-$ sudo apt install snapd
+sudo apt install snapd
 
 ## Install Chrome
 
-$ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-$ sudo dpkg -i google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 ## Install Docker
 
-$ sudo apt-get update
-$ sudo apt-get install \
+sudo apt-get update
+sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
-$ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-$ echo \
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-$ sudo apt-get update
-$ sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 ## Install Vscode
 
-$ sudo apt install software-properties-common apt-transport-https wget -y
-$ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-$ sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-$ sudo apt install code
-$ code --version
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+code --version
 
 ## Install pip3
-$ sudo apt install python3-pip -y
+sudo apt install python3-pip -y
 
 ## Install postman 
-$ sudo snap install postman -y
+sudo snap install postman -y
 
 ## Install slack
-$ sudo snap install slack -y
+sudo snap install slack -y
 
 ## Install spotfy
-$ sudo snap install spotify -y
+sudo snap install spotify -y
 
 ## Install discord
-$ sudo snap install discord -y
+sudo snap install discord -y
 
